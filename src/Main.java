31e7a6f5086dda17c99f48d2d90b4f563086fb41
Timofei <t.a.maksimenko@ya.ru>
2021-11-29 08:33:04 +0300

    init directory

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 10000;
+        int bonusMile = 20;
+        int bonusMileValue = price / bonusMile;
+        System.out.println("Количество миль полученных за перелет: " + bonusMileValue);
+    }
+}
